43d7246a32c6760a63a68ab8476d967616c87413
Sodium11 <Sodium11.for.gitserver@gmail.com>
2019-05-26 23:49:42 +0900

    Cursor move normalized

--- texn.c
+++ texn.c
@@ -4,7 +4,7 @@
 
 #define WIDTH 50
 #define HEIGHT 38
-#define MAP_P(x,y) (x+y*CGR_getWidth())
+#define MAP_P(x,y) ((x)+(y)*CGR_getWidth())
 #define DATA_SIZE WIDTH*HEIGHT
 #define LINE 100
 
@@ -58,8 +58,8 @@ return ed;
 }
 
 void update(editordata *ed){
-for (int y=0;y<ed->height;y++)
-for(int x=0;x<ed->width;x++)
+for (int y=0;y<(ed->txt).linenum;y++)
+for(int x=0;x<(ed->txt).linesize[y];x++)
 CGR_setChar(x,y,(ed->txt).data[MAP_P(x,y)]);
 return;
 }
@@ -81,13 +81,27 @@ case 'd':
 break;
 case 'r':
 	CGR_setChar(ed->x,ed->y,(ed->txt).data[MAP_P(ed->x,ed->y)]);
-        if((ed->x)<(ed->txt).linesize[ed->y])
-        (ed->x)--;
+        //update(ed);
+	if((ed->x)>=(ed->txt).linesize[ed->y]){
+		if(ed->y<(ed->txt).linenum){
+		moveCursor('d',ed);
+		ed->x=0;
+		}
+	}else{
+	(ed->x)++;
+	}
 break;
 case 'l':
 	CGR_setChar(ed->x,ed->y,(ed->txt).data[MAP_P(ed->x,ed->y)]);
-        if(ed->x>0)
-        (ed->x)--;
+        //update(ed);
+        if((ed->x)<=0){
+		if(ed->y>0){
+		moveCursor('u',ed);
+		ed->x=(ed->txt.linesize[ed->y]);
+		}
+	}else{
+	(ed->x)--;
+	}
 break;
 
 default:
@@ -107,12 +121,32 @@ return 1;
 return 0;
 }
 
+int texnload(FILE *fp,texndata *txt){
+int x=0,y=0;
+char ch;
+while((ch=fgetc(fp))!=EOF) {
+	printf("%c",ch);
+	if(ch=='\n'||ch=='\r'){
+	x=0;
+	y++;
+	}else{
+	txt->data[MAP_P(x,y)]=ch;
+	txt->linesize[y]++;
+	x++;
+	}
+}
+txt->linenum=y;
+return 0;
+}
+
 int texnsave(FILE *fp,texndata txt){
 int sum=0;
-for(int i=0;i<LINE;i++){
+for(int i=0;i<txt.linenum;i++){
+//printf("%d,",txt.linesize[i]);
 sum+=txt.linesize[i];
 for(int j=0;j<txt.linesize[i];j++)
 fprintf(fp,"%c",txt.data[MAP_P(j,i)]);
+fprintf(fp,"\n");
 }
 return sum;
 }
@@ -121,16 +155,27 @@ int main(int argc,char ** argv){
 CGR_init(WIDTH,HEIGHT);
 CGR_setChar(0,0,CURSOR);
 CGR_draw();
-FILE *fp;
-if(argc>=2)
-fp=fopen(argv[1],"w");
-else
-fp=fopen("output.txt","w");
+
 const int wid=CGR_getWidth();
 const int hei=CGR_getHeight();
-int p=0;
 editordata ed=emptyED(wid,hei);
-texndata txt=ed.txt;
+
+FILE *fp;
+if(argc>=2){
+fp=fopen(argv[1],"r");
+	if(fp){
+	texnload(fp,&(ed.txt));
+	update(&ed);
+/*	for(int i=0;i<ed.txt.linenum;i++)
+	for(int j=0;j<ed.txt.linesize[i];j++)
+	CGR_setChar(j,i,ed.txt.data[MAP_P(j,i)]);
+*/
+	CGR_draw();
+	}
+fclose(fp);
+}
+int p=0;
+//texndata txt=ed.txt;
 //main loop
 while (1){
 	char input=rawinput();
@@ -139,14 +184,17 @@ while (1){
 	break;
 
 	if(input==SAVE_KEY){
-	texnsave(fp,txt);
+	FILE *savefile=fopen("output.txt","w");
+	int f_size=texnsave(savefile,ed.txt);
+//	printf("%d",f_size);
+	fclose(savefile);
 	//fprintf(fp,"%s\n",txt.data);
 	}
 	if(input==ENTER_KEY){
 		if(ed.y<hei){
-		txt.data[MAP_P(ed.x,ed.y)]='\n';
-		txt.linesize[ed.y]++;
-		txt.linenum++;
+//		txt.data[MAP_P(ed.x,ed.y)]='\n';
+//		txt.linesize[ed.y]++;
+		ed.txt.linenum++;
 		moveCursor('d',&ed);
 		ed.x=0;
 		}
@@ -158,7 +206,7 @@ while (1){
 		moveCursor('l',&ed);
 		for (int i=ed.x;i<ed.txt.linesize[ed.y];i++)
 			ed.txt.data[MAP_P(i,ed.y)]=ed.txt.data[MAP_P(i+1,ed.y)];
-		ed.txt.data[MAP_P(ed.txt.linesize[ed.y]-1,ed.y)]='\0';
+		ed.txt.data[MAP_P((ed.txt.linesize[ed.y]-1),(ed.y))]='\0';
 		ed.txt.linesize[ed.y]--;
 		}
 	}
@@ -171,20 +219,20 @@ char ctlkey=rawinput();
 		moveCursor('u',&ed);
 	}
 	if(ctlkey==ARROW_DOWN){
-		if(ed.y<(txt.linenum)){
+		if(ed.y<(ed.txt.linenum)){
 			if(ed.y>hei-1)
 			ed.y=hei-1;
 		moveCursor('d',&ed);
 		}
 	}
 	if(ctlkey==ARROW_RIGHT){
-		if(ed.x<(ed.txt).linesize[ed.y]){
+		if(ed.x<=(ed.txt).linesize[ed.y]){
 			//ed.x++;
 			moveCursor('r',&ed);
 		}
 	}
 	if(ctlkey==ARROW_LEFT){
-		if(ed.x>0){
+		if(ed.x>=0){
 			//ed.x--;
 			moveCursor('l',&ed);
 		}
@@ -192,16 +240,14 @@ char ctlkey=rawinput();
 }
 	if(input>=0x20&&input<=0x7E){
 	CGR_setChar(ed.x,ed.y,input);
-	txt.data[MAP_P(ed.x,ed.y)]=input;
-	ed.x++;
-	txt.linesize[ed.y]++;
+	ed.txt.data[MAP_P(ed.x,ed.y)]=input;
+	ed.txt.linesize[ed.y]++;
+	moveCursor('r',&ed);
 	}
 	CGR_setChar(ed.x,ed.y,CURSOR);
 	CGR_draw();
 }
 
-
-fclose(fp);
 CGR_end();
 RawModeOff();
 return 0;
